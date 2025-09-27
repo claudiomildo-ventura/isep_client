96f54ad70b15d62fc8d4380d80ebbbbe8af1f6d9
Claudiomildo Ventura <claudiomildo@hotmail.com>
2025-09-26 17:57:33 -0700

    Enhance archetype component layout and add title component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {ReactiveFormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { ArchetypeComponent } from './components/archetype/archetype.component';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpclientService } from './core/services/httpclient.service';
+import {AppComponent} from './app.component';
+import {ArchetypeComponent} from './components/archetype/archetype.component';
+import {TitleComponent} from './components/title/title.component';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpclientService} from './core/services/httpclient.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ArchetypeComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    ReactiveFormsModule.withConfig({ warnOnNgModelWithFormControl: 'always' })
-  ],
-  providers: [HttpclientService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent, ArchetypeComponent,
+        TitleComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'always'})
+    ],
+    providers: [HttpclientService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
